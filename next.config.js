f1e7d361de447240a9a2ad358636549ed047429f
keizerh1 <elezbi@outlook.fr>
2025-08-24 14:12:28 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
     domains: ['placeholder.pics', 'via.placeholder.com', 'picsum.photos', 'cdn.discordapp.com'],
   },
